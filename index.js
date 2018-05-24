cd7d24480a8a60d41ba6771b5f9366c40a2d4ec4
Eric Abenojar <abenoeric@gmail.com>
2018-05-24 13:55:21 -0700

    Fixed server

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.get("/", function(res,res){
   res.render("index")
 })
 
-var port = process.env.PORT || 8000;
+var port = process.env.PORT || 8080;
 //Set up server
-app.listen(port, console.log("APP STARTED ON SERVER 5000"));
+app.listen(port, console.log("APP STARTED ON SERVER 8080"));
